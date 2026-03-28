d6ce1c982d277105716ac805df07941fb18a56c5
moeezrashidd <moeezrashidd@github>
2026-03-28 23:29:23 +0500

    added 2 new projects

---
+++ practice/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using static BookManagementApp;
+
+class program{
+    static void Main(string[] args){
+        BookUI bookManagement = new BookUI();
+        bookManagement.bookMian(args);
+    }
+}
