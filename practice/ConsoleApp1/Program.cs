64f03a857671fef5382003d771273eb11384af16
Moeez Rashid <moeezrashidd@gmail.com>
2026-03-28 23:28:43 +0500

    Delete practice directory

--- practice/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using static BookManagementApp;
-
-class program{
-    static void Main(string[] args){
-        BookUI bookManagement = new BookUI();
-        bookManagement.bookMian(args);
-    }
-}
