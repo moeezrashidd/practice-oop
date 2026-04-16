dc88a40c5fe72230fcbdbf1edef3d4f3bd18ce2a
Moeez Rashid <moeezrashidd@gmail.com>
2026-04-16 23:38:58 +0500

    pre-mid-assignment

--- mid practice/question for all concepts.cs
+++ mid practice/question for all concepts.cs
@@ -1,40 +1,38 @@
 ﻿using System;
 
- class Student
-{	
-	public string name;
-	public int rollNumber;
-	private int marks;
+public class Student
+{
+    public string name;
+    public int rollNumber;
+    private int marks;
 
-	public int Marks { 
-		get { return marks; }
-
-		set
-		{
-			if (value <0 || value >100)
-			{
-				Console.WriteLine("Invalid marks. Marks should be between 0 and 100.");
-			}
-			else
-			{
-				marks = value;
+    public int Marks
+    {
+        get { return marks; }
+        set
+        {
+            if (value < 0 || value > 100)
+            {
+                Console.WriteLine("Invalid marks. Marks should be between 0 and 100.");
+            }
+            else
+            {
+                marks = value;
             }
         }
     }
 
-	public Student(string name, int rollNumber)
-	{
-		this.name = name;
-		this.rollNumber = rollNumber;
-		this.Marks = 0; // Default marks
-
+    public Student(string name, int rollNumber)
+    {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.Marks = 0; 
     }
 
-	public Student(string name, int rollNumber, int marks)
-	{
-		this.name = name;
-		this.rollNumber = rollNumber;
-		this.Marks = marks; // Use the property to set marks with validation
+    public Student(string name, int rollNumber, int marks)
+    {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.Marks = marks; 
     }
-
 }
