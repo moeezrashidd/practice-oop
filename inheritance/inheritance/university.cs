807c01682d8df0f18c64eba5c01334ee9bf9d5a3
Moeez Rashid <moeezrashidd@gmail.com>
2026-04-29 23:22:46 +0500

    polymorphisam files are added

--- inheritance/inheritance/university.cs
+++ inheritance/inheritance/university.cs
@@ -16,10 +16,23 @@ public class Person
 		Console.WriteLine($"Name: {name}, Age: {age}");
     }
 
-	public abstract void Role()
+	public abstract void Role(string rol)
 	{
-		string role = "Employee";
+		string role = rol;
 		Console.WriteLine("Role: " + role);
 	}
-	};
+
+}
+
+public class Employee : Person
+{
+	protected string salary;
+	public Employee(string name, int age, string salary) : base(name, age)
+	{
+		this.salary = salary;
+	}
+	public override void Role()
+	{
+		Console.WriteLine("Role: " + );
+	}
 }
