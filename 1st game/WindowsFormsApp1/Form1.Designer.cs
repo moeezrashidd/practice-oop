8634d406e40c02660885b7abea5720586a07d8b0
Moeez Rashid <moeezrashidd@gmail.com>
2026-07-05 17:55:01 +0500

    final commit on this repo

---
+++ 1st game/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerbox = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.playerbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerbox
+            // 
+            this.playerbox.BackColor = System.Drawing.Color.Transparent;
+            this.playerbox.Image = ((System.Drawing.Image)(resources.GetObject("playerbox.Image")));
+            this.playerbox.Location = new System.Drawing.Point(520, 403);
+            this.playerbox.Name = "playerbox";
+            this.playerbox.Size = new System.Drawing.Size(110, 100);
+            this.playerbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerbox.TabIndex = 0;
+            this.playerbox.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Interval = 30;
+            this.Player.Tick += new System.EventHandler(this.Player_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1109, 533);
+            this.Controls.Add(this.playerbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playerbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox playerbox;
+        private System.Windows.Forms.Timer Player;
+    }
+}
+
---
+++ 1st game/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using EZInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Player_Tick(object sender, EventArgs e)
+        {
+            if(Keyboard.IsKeyPressed(Key.RightArrow))
+            {
+             
+                playerbox.Left += 10;
+             
+            }
+
+
+            if (Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                playerbox.Left -=   10;
+            }
+
+            if (Keyboard.IsKeyPressed(Key.UpArrow))
+            {
+                playerbox.Top -= 10;
+            }
+
+            if (Keyboard.IsKeyPressed(Key.DownArrow))
+            {
+                playerbox.Top += 10;
+            }
+        }
+    }
+}
